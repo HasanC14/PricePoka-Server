06bd4f4a2dfd1ac2472bb3a686d94ca239fee43f
HasanC14 <dev.hasanchowdhury@gmail.com>
2025-05-16 11:10:44 +0600

    startech issue

--- src/app/modules/Scraper/scrapper.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { ScrapperController } from "./scraper.controller.js";
-const ScraperRoutes = express.Router();
-
-ScraperRoutes.get("/:product", ScrapperController.getAllScrape);
-
-export default ScraperRoutes
