048f20c4e6e91ddf33bdbbe246f2674af70fbfda
HasanC14 <dev.hasanchowdhury@gmail.com>
2025-05-18 09:48:41 +0600

    configured to trust proxies by default

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const crypto = require("crypto");
 const rateLimit = require("express-rate-limit");
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
